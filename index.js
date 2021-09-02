0900ee28131086b0bd15034725ab6f69395cd7a8
Ben Spotswood <spotswoodb@gmail.com>
2021-09-01 23:06:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(renderBooks)
 }
 
 function renderBooks(books) {
@@ -11,6 +13,15 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
